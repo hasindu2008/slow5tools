13fe3cb180d01b784b013fdbb5650f526c5ba509
Sasha Jenner <jenner.sasha@gmail.com>
2020-08-16 12:18:30 +1000

    Cleaning up error header and starting fast5toslow5

--- src/error.h
+++ src/error.h
@@ -2,7 +2,12 @@
 #define ERROR_H
 
 #include <errno.h>
+#include <stddef.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
+#define STDERR_PREFIX "[%s] "
 #define WARNING_PREFIX "[%s::WARNING]\033[1;33m "
 #define ERROR_PREFIX "[%s::ERROR]\033[1;31m "
 #define INFO_PREFIX "[%s::INFO]\033[1;34m "
@@ -10,78 +15,66 @@
 #define DEBUG_PREFIX "[%s::DEBUG]\033[1;35m " 
 #define NO_COLOUR "\033[0m\n"
 
-#define STDERR(arg, ...)                                                      \
-    fprintf(stderr, "[%s] " arg "\n", __func__,                        \
-            __VA_ARGS__)
-#define WARNING(arg, ...)                                                      \
-    fprintf(stderr, WARNING_PREFIX arg NO_COLOUR, __func__,      \
-            __VA_ARGS__)
-#define ERROR(arg, ...)                                                        \
-    fprintf(stderr, ERROR_PREFIX arg NO_COLOUR, __func__,        \
-            __VA_ARGS__)
-#define INFO(arg, ...)                                                         \
-    fprintf(stderr, INFO_PREFIX arg NO_COLOUR, __func__,         \
-            __VA_ARGS__)
-#define SUCCESS(arg, ...)                                                      \
-    fprintf(stderr, SUCCESS_PREFIX arg NO_COLOUR, __func__,      \
-            __VA_ARGS__)
-#define DEBUG(arg, ...)                                                        \
-    fprintf(stderr,                                                            \
-            DEBUG_PREFIX "Error occured at %s:%d. " arg NO_COLOUR,  \
-            __func__, __FILE__, __LINE__ - 2, __VA_ARGS__)
+#define STDERR(msg, ...) fprintf(stderr, STDERR_PREFIX msg "\n", __func__, __VA_ARGS__)
+#define WARNING(msg, ...) fprintf(stderr, WARNING_PREFIX msg NO_COLOUR, __func__, __VA_ARGS__)
+#define ERROR(msg, ...) fprintf(stderr, ERROR_PREFIX msg NO_COLOUR, __func__, __VA_ARGS__)
+#define INFO(msg, ...) fprintf(stderr, INFO_PREFIX msg NO_COLOUR, __func__, __VA_ARGS__)
+#define SUCCESS(msg, ...) fprintf(stderr, SUCCESS_PREFIX msg NO_COLOUR, __func__, __VA_ARGS__)
+#define DEBUG(msg, ...) fprintf(stderr, DEBUG_PREFIX "Error occured at %s:%d. " msg NO_COLOUR, \
+                                __func__, __FILE__, __LINE__ - 1, __VA_ARGS__) // TODO why -2
 
-#define MALLOC_CHK(ret) malloc_chk((void*)ret, __func__, __FILE__, __LINE__ - 1)
-#define F_CHK(ret, filename)                                                   \
-    f_chk((void*)ret, __func__, __FILE__, __LINE__ - 1, filename);
-#define NULL_CHK(ret) null_chk((void*)ret, __func__, __FILE__, __LINE__ - 1)
+#define MALLOC_CHK(ret) malloc_chk((void*) ret, __func__, __FILE__, __LINE__ - 1)
+#define F_CHK(ret, filename) f_chk((void*) ret, __func__, __FILE__, __LINE__ - 1, filename)
+#define NULL_CHK(ret) null_chk((void*) ret, __func__, __FILE__, __LINE__ - 1)
 #define NEG_CHK(ret) neg_chk(ret, __func__, __FILE__, __LINE__ - 1)
 
-static inline void malloc_chk(void* ret, const char* func, const char* file,
+static inline void malloc_chk(void* ret, const char* func, const char* file, 
                               int line) {
-    if (ret != NULL)
-        return;
-    fprintf(
-        stderr,
-        "[%s::ERROR]\033[1;31m Failed to allocate memory : "
-        "%s.\033[0m\n[%s::DEBUG]\033[1;35m Error occured at %s:%d. Try with a small batchsize (-K and/or -B options) to reduce the peak memory\033[0m\n\n",
-        func, strerror(errno), func, file, line);
-    exit(EXIT_FAILURE);
+    if (ret == NULL) {
+        fprintf(stderr, ERROR_PREFIX "Failed to allocate memory : %s." NO_COLOUR,
+                func, strerror(errno));
+        fprintf(stderr, DEBUG_PREFIX "Error occured at %s:%d. Try with a small batchsize "
+                "(-K and/or -B options) to reduce the peak memory.\n" NO_COLOUR,
+                func, file, line);
+
+        exit(EXIT_FAILURE);
+    }
 }
 
 static inline void f_chk(void* ret, const char* func, const char* file,
                          int line, const char* fopen_f) {
-    if (ret != NULL)
-        return;
-    fprintf(
-        stderr,
-        "[%s::ERROR]\033[1;31m Failed to open %s : "
-        "%s.\033[0m\n[%s::DEBUG]\033[1;35m Error occured at %s:%d.\033[0m\n\n",
-        func, fopen_f, strerror(errno), func, file, line);
-    exit(EXIT_FAILURE);
+    if (ret == NULL) {
+        fprintf(stderr, ERROR_PREFIX "Failed to open %s : %s." NO_COLOUR,
+                func, fopen_f, strerror(errno));
+        fprintf(stderr, DEBUG_PREFIX "Error occured at %s:%d.\n" NO_COLOUR,
+                func, file, line);
+
+        exit(EXIT_FAILURE);
+    }
 }
 
-// Die on error. Print the error and exit if the return value of the previous function NULL
 static inline void null_chk(void* ret, const char* func, const char* file,
                             int line) {
-    if (ret != NULL)
-        return;
-    fprintf(stderr,
-            "[%s::ERROR]\033[1;31m %s.\033[0m\n[%s::DEBUG]\033[1;35m Error "
-            "occured at %s:%d.\033[0m\n\n",
-            func, strerror(errno), func, file, line);
-    exit(EXIT_FAILURE);
+    if (ret == NULL) {
+        fprintf(stderr, ERROR_PREFIX "Unexpected NULL value : %s." NO_COLOUR,
+                func, strerror(errno));
+        fprintf(stderr, DEBUG_PREFIX "Error occured at %s:%d.\n" NO_COLOUR,
+                func, file, line);
+
+        exit(EXIT_FAILURE);
+    }
 }
 
-// Die on error. Print the error and exit if the return value of the previous function is -1
 static inline void neg_chk(int ret, const char* func, const char* file,
                            int line) {
-    if (ret >= 0)
-        return;
-    fprintf(stderr,
-            "[%s::ERROR]\033[1;31m %s.\033[0m\n[%s::DEBUG]\033[1;35m Error "
-            "occured at %s:%d.\033[0m\n\n",
-            func, strerror(errno), func, file, line);
-    exit(EXIT_FAILURE);
+    if (ret < 0) {
+        fprintf(stderr, ERROR_PREFIX "Unexpected negative value : %s." NO_COLOUR,
+                func, strerror(errno));
+        fprintf(stderr, DEBUG_PREFIX "Error occured at %s:%d.\n" NO_COLOUR,
+                func, file, line);
+
+        exit(EXIT_FAILURE);
+    }
 }
 
 #endif
--- src/fast5toslow5.c
+++ src/fast5toslow5.c
@@ -1,26 +1,40 @@
+#include "error.h"
+#include <signal.h>
+#include <unistd.h>
+
 #ifdef HAVE_EXECINFO_H
     #include <execinfo.h>
 #endif
 
+// Backtrace buffer threshold of functions
 #define BT_BUF_SIZE (100)
+// Number of backtrace calls from the segmentation fault source to the handler
+#define SEG_FAULT_BT_SIZE (2)
+#define SEG_FAULT_MSG "I regret to inform that a segmentation fault occurred. " \
+                      "But at least it is better than a wrong answer."
 
 // Segmentation fault handler
-void sig_handler(int sig) {
+void segv_handler(int sig) {
 
-    ERROR("I regret to inform that a segmentation fault occurred. But at least "
-          "it is better than a wrong answer%s",
-          ".");
+    ERROR(SEG_FAULT_MSG "%s", "");
 
 #ifdef HAVE_EXECINFO_H
     void *buffer[BT_BUF_SIZE];
     int size = backtrace(buffer, BT_BUF_SIZE);
-    fprintf(stderr,
-            "[%s::DEBUG]\033[1;35m Here is the backtrace in case it is of any "
-            "use:\n",
+    NEG_CHK(size);
+    fprintf(stderr, DEBUG_PREFIX "Here is the backtrace:\n",
             __func__);
-    backtrace_symbols_fd(&buffer[2], size - 1, STDERR_FILENO);
-    fprintf(stderr, "\033[0m\n");
+    backtrace_symbols_fd(buffer + SEG_FAULT_BT_SIZE, size - SEG_FAULT_BT_SIZE, STDERR_FILENO);
+    fprintf(stderr, NO_COLOUR);
 #endif
 
     exit(EXIT_FAILURE);
+}
+
+int main(void) {
+    if (signal(SIGSEGV, segv_handler) == SIG_ERR) {
+        WARNING("Segmentation fault signal handler failed to be setup.%s", "");
+    }
+
+    return 0;
 }
