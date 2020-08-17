9dd1c1386f2bdaca0ccdf6493636393d1dec8ff8
Sasha Jenner <jenner.sasha@gmail.com>
2020-08-17 12:26:56 +1000

    Argument handler
    Takes arguments using getopt_long.

--- src/fast5toslow5.c
+++ src/fast5toslow5.c
@@ -1,25 +1,28 @@
 #include "error.h"
 #include <signal.h>
 #include <unistd.h>
+#include <getopt.h>
+#include <stdbool.h>
 
 #ifdef HAVE_EXECINFO_H
     #include <execinfo.h>
 #endif
 
-#define USAGE_MSG \
+#define USAGE_MSG "Usage: %s [OPTION]... [FAST5_FILE/DIR]...\n"
+#define HELP_SMALL_MSG "Try '%s --help' for more information.\n"
+#define HELP_LARGE_MSG \
+    USAGE_MSG \
     "Convert fast5 file(s) to slow5.\n" \
     "\n" \
-    "Usage: fast5toslow5 [OPTIONS] [FAST5_FILE/DIR_1 ...]\n" \
-    "\n" \
     "OPTIONS:\n" \
-    "    -d [NUM], --max-depth=[NUM]\n" \
+    "    -d|--max-depth [NUM]\n" \
     "        Sets the maximum depth to search directories for fast5 files.\n" \
     "        Default: No maximum depth.\n" \
     "        E.g. NUM=1: Reads the files within a specified directory but\n" \
     "        not those within subdirectories.\n" \
     "\n" \
     "    -h|--help\n" \
-    "        Prints helpful message.\n" \
+    "        Prints this message.\n" \
     "\n" \
     "    -o|--output [SLOW5_FILE]\n" \
     "        Outputs slow5 contents to SLOW5_FILE.\n" \
@@ -35,6 +38,7 @@
 #define SEG_FAULT_MSG "I regret to inform that a segmentation fault occurred. " \
                       "But at least it is better than a wrong answer."
 
+
 // Segmentation fault handler
 void segv_handler(int sig) {
 
@@ -46,22 +50,66 @@ void segv_handler(int sig) {
     NEG_CHK(size);
     fprintf(stderr, DEBUG_PREFIX "Here is the backtrace:\n",
             __func__);
-    backtrace_symbols_fd(buffer + SEG_FAULT_BT_SIZE, size - SEG_FAULT_BT_SIZE, STDERR_FILENO);
+    backtrace_symbols_fd(buffer + SEG_FAULT_BT_SIZE, size - SEG_FAULT_BT_SIZE, 
+                         STDERR_FILENO);
     fprintf(stderr, NO_COLOUR);
 #endif
 
     exit(EXIT_FAILURE);
 }
 
-// Output usage to file
-void print_usage(FILE *fp_help) {
-    fprintf(fp_help, USAGE_MSG);
-}
-
 int main(int argc, char **argv) {
     // Setup segmentation fault handler
     if (signal(SIGSEGV, segv_handler) == SIG_ERR) {
         WARNING("Segmentation fault signal handler failed to be setup.%s", "");
+    }
+
+    static struct option long_options[] = {
+        {"max-depth", required_argument, NULL, 'd' },
+        {"help", no_argument, NULL, 'h' },
+        {"output", required_argument, NULL, 'o' },
+        {"verbose", no_argument, NULL, 'v' },
+        {NULL, 0, NULL, 0 }
+    };
+
+    int max_depth = -1;
+    FILE *f_out = stdout;
+    bool verbose = false;
+
+    char *arg_max_depth = NULL;
+    char *arg_fname_out = NULL;
+
+    char opt;
+    while ((opt = getopt_long(argc, argv, "d:ho:v", long_options, NULL)) != -1) {
+        switch (opt) {
+            case 'd':
+                arg_max_depth = optarg;
+                break;
+            case 'h':
+                fprintf(stdout, HELP_LARGE_MSG, argv[0]);
+                return EXIT_SUCCESS;
+            case 'o':
+                arg_fname_out = optarg;
+                break;
+            case 'v':
+                verbose = true;
+                break;
+            default: // case '?' 
+                fprintf(stderr, HELP_SMALL_MSG, argv[0]);
+                return EXIT_FAILURE;
+        }
+    }
+
+    // Check for remaining files to parse
+    if (optind >= argc) {
+        MESSAGE("expected fast5 files or directories%s", "");
+        fprintf(stderr, HELP_SMALL_MSG, argv[0]);
+        return EXIT_FAILURE;
+    }
+    
+    // Parse maximum depth argument
+    if (arg_max_depth != NULL) {
+          
     }
 
     return EXIT_SUCCESS;
