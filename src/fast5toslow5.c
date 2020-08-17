d0c3155c9945ef103ae096e1dfe9d5ce688724c6
Sasha Jenner <jenner.sasha@gmail.com>
2020-08-17 17:45:24 +1000

    Parsing depth option

--- src/fast5toslow5.c
+++ src/fast5toslow5.c
@@ -3,6 +3,8 @@
 #include <unistd.h>
 #include <getopt.h>
 #include <stdbool.h>
+#include <string.h>
+#include <ctype.h>
 
 #ifdef HAVE_EXECINFO_H
     #include <execinfo.h>
@@ -15,20 +17,22 @@
     "Convert fast5 file(s) to slow5.\n" \
     "\n" \
     "OPTIONS:\n" \
-    "    -d|--max-depth [NUM]\n" \
+    "    -d, --max-depth=[NUM]\n" \
     "        Sets the maximum depth to search directories for fast5 files.\n" \
+    "        NUM must be a non-negative integer.\n" \
     "        Default: No maximum depth.\n" \
+    "\n" \
     "        E.g. NUM=1: Reads the files within a specified directory but\n" \
     "        not those within subdirectories.\n" \
     "\n" \
-    "    -h|--help\n" \
+    "    -h, --help\n" \
     "        Prints this message.\n" \
     "\n" \
-    "    -o|--output [SLOW5_FILE]\n" \
+    "    -o, --output=[SLOW5_FILE]\n" \
     "        Outputs slow5 contents to SLOW5_FILE.\n" \
     "        Default: Stdout.\n" \
     "\n" \
-    "    -v|--verbose\n" \
+    "    -v, --verbose\n" \
     "        Output more information while converting.\n"
 
 // Backtrace buffer threshold of functions
@@ -72,14 +76,17 @@ int main(int argc, char **argv) {
         {NULL, 0, NULL, 0 }
     };
 
-    int max_depth = -1;
+    // Default options
+    long max_depth = -1;
     FILE *f_out = stdout;
     bool verbose = false;
 
+    // Input arguments
     char *arg_max_depth = NULL;
     char *arg_fname_out = NULL;
 
     char opt;
+    // Parse options
     while ((opt = getopt_long(argc, argv, "d:ho:v", long_options, NULL)) != -1) {
         switch (opt) {
             case 'd':
@@ -100,16 +107,48 @@ int main(int argc, char **argv) {
         }
     }
 
+    // Parse maximum depth argument
+    if (arg_max_depth != NULL) {
+
+        // Check it is a number
+        
+        // Cannot be empty 
+        size_t arg_len = strlen(arg_max_depth);
+        if (arg_len == 0) {
+            MESSAGE("invalid max depth -- '%s'", arg_max_depth);
+            fprintf(stderr, HELP_SMALL_MSG, argv[0]);
+            return EXIT_FAILURE;
+        }
+
+        for (size_t i = 0; i < arg_len; ++ i) {
+            // Not a digit and first char is not a '+'
+            if (!isdigit((unsigned char) arg_max_depth[i]) && 
+                    !(i == 0 && arg_max_depth[i] == '+')) {
+                MESSAGE("invalid max depth -- '%s'", arg_max_depth);
+                fprintf(stderr, HELP_SMALL_MSG, argv[0]);
+                return EXIT_FAILURE;
+            }
+        }
+
+        // Parse argument
+        max_depth = strtol(arg_max_depth, NULL, 10);
+        // Check for overflow
+        if (errno == ERANGE) {
+            WARNING("Overflow of max depth '%s'. Setting to %ld instead.", 
+                    arg_max_depth, max_depth);
+        }
+    }
+
+    // Parse output argument
+    if (arg_fname_out != NULL) {
+
+    }
+
     // Check for remaining files to parse
     if (optind >= argc) {
         MESSAGE("expected fast5 files or directories%s", "");
         fprintf(stderr, HELP_SMALL_MSG, argv[0]);
         return EXIT_FAILURE;
-    }
-    
-    // Parse maximum depth argument
-    if (arg_max_depth != NULL) {
-          
     }
 
     return EXIT_SUCCESS;
