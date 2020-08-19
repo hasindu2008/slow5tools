697f0b68e90da67f4fe55b4b457af588baabdf9a
Sasha Jenner <jenner.sasha@gmail.com>
2020-08-19 15:15:56 +1000

    Added nicer debug statement

--- src/error.h
+++ src/error.h
@@ -11,20 +11,23 @@
 
 #define STDERR_PREFIX "[%s] "
 #define MESSAGE_PREFIX "%s: "
+#define DEBUG_PREFIX "%s [%s:%s:%d]: \033[1;35m"
 #define WARNING_PREFIX "[%s::WARNING]\033[1;33m "
 #define ERROR_PREFIX "[%s::ERROR]\033[1;31m "
 #define INFO_PREFIX "[%s::INFO]\033[1;34m "
 #define SUCCESS_PREFIX "[%s::SUCCESS]\033[1;32m "
-#define DEBUG_PREFIX "[%s::DEBUG]\033[1;35m " 
+#define DEBUG2_PREFIX "[%s::DEBUG]\033[1;35m " 
 #define NO_COLOUR "\033[0m\n"
 
 #define STDERR(msg, ...) fprintf(stderr, STDERR_PREFIX msg "\n", __func__, __VA_ARGS__)
 #define MESSAGE(msg, ...) fprintf(stderr, MESSAGE_PREFIX msg "\n", argv[0], __VA_ARGS__)
+#define DEBUG(msg, ...) fprintf(stderr, DEBUG_PREFIX msg NO_COLOUR, argv[0], __FILE__, __func__, \
+                                __LINE__, __VA_ARGS__);
 #define WARNING(msg, ...) fprintf(stderr, WARNING_PREFIX msg NO_COLOUR, __func__, __VA_ARGS__)
 #define ERROR(msg, ...) fprintf(stderr, ERROR_PREFIX msg NO_COLOUR, __func__, __VA_ARGS__)
 #define INFO(msg, ...) fprintf(stderr, INFO_PREFIX msg NO_COLOUR, __func__, __VA_ARGS__)
 #define SUCCESS(msg, ...) fprintf(stderr, SUCCESS_PREFIX msg NO_COLOUR, __func__, __VA_ARGS__)
-#define DEBUG(msg, ...) fprintf(stderr, DEBUG_PREFIX "Error occured at %s:%d. " msg NO_COLOUR, \
+#define DEBUG2(msg, ...) fprintf(stderr, DEBUG2_PREFIX "Error occured at %s:%d. " msg NO_COLOUR, \
                                 __func__, __FILE__, __LINE__ - 1, __VA_ARGS__) // TODO why -2
 
 #define MALLOC_CHK(ret) malloc_chk((void*) ret, __func__, __FILE__, __LINE__ - 1)
@@ -37,7 +40,7 @@ static inline void malloc_chk(void* ret, const char* func, const char* file,
     if (ret == NULL) {
         fprintf(stderr, ERROR_PREFIX "Failed to allocate memory : %s." NO_COLOUR,
                 func, strerror(errno));
-        fprintf(stderr, DEBUG_PREFIX "Error occured at %s:%d. Try with a small batchsize "
+        fprintf(stderr, DEBUG2_PREFIX "Error occured at %s:%d. Try with a small batchsize "
                 "(-K and/or -B options) to reduce the peak memory." NO_COLOUR,
                 func, file, line);
 
@@ -50,7 +53,7 @@ static inline void f_chk(void* ret, const char* func, const char* file,
     if (ret == NULL) {
         fprintf(stderr, ERROR_PREFIX "Failed to open %s : %s." NO_COLOUR,
                 func, fopen_f, strerror(errno));
-        fprintf(stderr, DEBUG_PREFIX "Error occured at %s:%d." NO_COLOUR,
+        fprintf(stderr, DEBUG2_PREFIX "Error occured at %s:%d." NO_COLOUR,
                 func, file, line);
 
         exit(EXIT_FAILURE);
@@ -62,7 +65,7 @@ static inline void null_chk(void* ret, const char* func, const char* file,
     if (ret == NULL) {
         fprintf(stderr, ERROR_PREFIX "Unexpected NULL value : %s." NO_COLOUR,
                 func, strerror(errno));
-        fprintf(stderr, DEBUG_PREFIX "Error occured at %s:%d." NO_COLOUR,
+        fprintf(stderr, DEBUG2_PREFIX "Error occured at %s:%d." NO_COLOUR,
                 func, file, line);
 
         exit(EXIT_FAILURE);
@@ -74,7 +77,7 @@ static inline void neg_chk(int ret, const char* func, const char* file,
     if (ret < 0) {
         fprintf(stderr, ERROR_PREFIX "Unexpected negative value : %s." NO_COLOUR,
                 func, strerror(errno));
-        fprintf(stderr, DEBUG_PREFIX "Error occured at %s:%d." NO_COLOUR,
+        fprintf(stderr, DEBUG2_PREFIX "Error occured at %s:%d." NO_COLOUR,
                 func, file, line);
 
         exit(EXIT_FAILURE);
