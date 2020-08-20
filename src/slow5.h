5f2982d5add14e40a3dc87216bacfc641dd1d0d2
Sasha Jenner <jenner.sasha@gmail.com>
2020-08-20 12:28:51 +1000

    New macro for message before return

--- src/slow5.h
+++ src/slow5.h
@@ -14,6 +14,8 @@
 //required for eventalign
 //#include <vector>
 
+#include "error.h"
+
 #define SLOW5_VERSION "0.0"
 
 struct program_meta {
@@ -25,5 +27,23 @@ struct command {
     char *name;
     int (*main)(int, char **, struct program_meta *);
 };
+
+# define EXIT_MSG(exit_code, argv, meta) exit_msg(exit_code, argv, meta, __FILE__, __func__, __LINE__);
+
+static inline void exit_msg(int exit_code, char **argv, struct program_meta *meta,
+                            const char *file, const char *func, int line) {
+    if (meta != NULL) {
+        if (meta->verbose) {
+            VERBOSE("exiting with %s", 
+                    exit_code == EXIT_SUCCESS ? "SUCCESS" : 
+                    exit_code == EXIT_FAILURE ? "FAILURE" :
+                    "UNKNOWN OUTCOME");
+        }
+        if (meta->debug) {
+            fprintf(stderr, DEBUG_PREFIX "exit code %d" NO_COLOUR,
+                    file, func, line, exit_code);
+        }
+    }
+}
 
 #endif
