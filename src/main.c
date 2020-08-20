65c8e07fa429993922ee7d4e4158c32af6f45529
Sasha Jenner <jenner.sasha@gmail.com>
2020-08-20 17:14:03 +1000

    Restructured and realtime works
    Return at the very end

--- src/main.c
+++ src/main.c
@@ -12,7 +12,7 @@
 #include <signal.h>
 #include <getopt.h>
 #include <stdbool.h>
-//#include "slow5misc.h" // TODO uncomment
+#include "slow5misc.h"
 #include "slow5.h"
 #include "error.h"
 
@@ -82,13 +82,17 @@ void segv_handler(int sig) {
 
 int main(int argc, char **argv){
 
+    // Initial time
+    double init_realtime = realtime();
+
+    // Return code
+    int ret = EXIT_SUCCESS;
+
     // Default options
     struct program_meta meta = {
         .debug = false,
         .verbose = false
     };
-
-    //double realtime0 = realtime(); // TODO uncomment
 
     // Setup segmentation fault handler
     if (signal(SIGSEGV, segv_handler) == SIG_ERR) {
@@ -98,178 +102,154 @@ int main(int argc, char **argv){
     // No arguments given
     if (argc <= 1) {
         fprintf(stderr, USAGE_MSG HELP_SMALL_MSG, argv[0], argv[0]);
-        EXIT_MSG(EXIT_FAILURE, argv, &meta);
-        return EXIT_FAILURE;
-    }
+        ret = EXIT_FAILURE;
 
-    static struct command cmds[] = {
-        {"f2s", f2s_main},
-        {"fastt", fastt_main}
-    };
-    static size_t num_cmds = sizeof (cmds) / sizeof (struct command);
+    } else {
+        static struct command cmds[] = {
+            {"f2s", f2s_main},
+            {"fastt", fastt_main}
+        };
+        static size_t num_cmds = sizeof (cmds) / sizeof (struct command);
 
-    static struct option long_opts[] = {
-        {"debug", no_argument, NULL, 'd' },
-        {"help", no_argument, NULL, 'h' },
-        {"verbose", no_argument, NULL, 'v'},
-        {"version", no_argument, NULL, 'V'},
-        {NULL, 0, NULL, 0 }
-    };
+        static struct option long_opts[] = {
+            {"debug", no_argument, NULL, 'd' },
+            {"help", no_argument, NULL, 'h' },
+            {"verbose", no_argument, NULL, 'v'},
+            {"version", no_argument, NULL, 'V'},
+            {NULL, 0, NULL, 0 }
+        };
 
-    char opt;
-    // Parse options up to first non-option argument (command)
-    while ((opt = getopt_long(argc, argv, "+dhvV", long_opts, NULL)) != -1) {
+        char opt;
+        bool break_flag = false;
+        // Parse options up to first non-option argument (command)
+        while (!break_flag && (opt = getopt_long(argc, argv, "+dhvV", long_opts, NULL)) != -1) {
 
-        if (meta.debug) {
-            DEBUG("opt='%c', optarg=\"%s\", optind=%d, opterr=%d, optopt='%c'",
-                  opt, optarg, optind, opterr, optopt);
-        }
+            if (meta.debug) {
+                DEBUG("opt='%c', optarg=\"%s\", optind=%d, opterr=%d, optopt='%c'",
+                      opt, optarg, optind, opterr, optopt);
+            }
 
-        switch (opt) {
-            case 'd':
-                // Print arguments
-                if (!meta.debug) {
-                    if (meta.verbose) {
-                        VERBOSE("printing the arguments given%s","");
-                    }
-
-                    fprintf(stderr, DEBUG_PREFIX "argv=[",
-                            __FILE__, __func__, __LINE__);
-                    for (int i = 0; i < argc; ++ i) {
-                        fprintf(stderr, "\"%s\"", argv[i]);
-                        if (i == argc - 1) {
-                            fprintf(stderr, "]");
-                        } else {
-                            fprintf(stderr, ", ");
+            switch (opt) {
+                case 'd':
+                    // Print arguments
+                    if (!meta.debug) {
+                        if (meta.verbose) {
+                            VERBOSE("printing the arguments given%s","");
                         }
+
+                        fprintf(stderr, DEBUG_PREFIX "argv=[",
+                                __FILE__, __func__, __LINE__);
+                        for (int i = 0; i < argc; ++ i) {
+                            fprintf(stderr, "\"%s\"", argv[i]);
+                            if (i == argc - 1) {
+                                fprintf(stderr, "]");
+                            } else {
+                                fprintf(stderr, ", ");
+                            }
+                        }
+                        fprintf(stderr, NO_COLOUR);
+
+                        meta.debug = true;
                     }
-                    fprintf(stderr, NO_COLOUR);
 
-                    meta.debug = true;
-                }
+                    break;
+                case 'h':
+                    if (meta.verbose) {
+                        VERBOSE("displaying large help message%s","");
+                    }
+                    fprintf(stdout, HELP_LARGE_MSG, argv[0], argv[0]);
 
-                break;
-            case 'h':
-                if (meta.verbose) {
-                    VERBOSE("displaying large help message%s","");
-                }
-                fprintf(stdout, HELP_LARGE_MSG, argv[0], argv[0]);
+                    ret = EXIT_SUCCESS;
+                    break_flag = true;
+                    break;
+                case 'v':
+                    meta.verbose = true;
+                    break;
+                case 'V':
+                    if (meta.verbose) {
+                        VERBOSE("displaying version information%s","");
+                    }
+                    fprintf(stdout, VERSION_MSG, argv[0]);
 
-                EXIT_MSG(EXIT_SUCCESS, argv, &meta);
-                return EXIT_SUCCESS;
-            case 'v':
-                meta.verbose = true;
-                break;
-            case 'V':
-                if (meta.verbose) {
-                    VERBOSE("displaying version information%s","");
-                }
-                fprintf(stdout, VERSION_MSG, argv[0]);
+                    ret = EXIT_SUCCESS;
+                    break_flag = true;
+                    break;
 
-                EXIT_MSG(EXIT_SUCCESS, argv, &meta);
-                return EXIT_SUCCESS;
-
-            default: // case '?'
-                fprintf(stderr, HELP_SMALL_MSG, argv[0]);
-                EXIT_MSG(EXIT_FAILURE, argv, &meta);
-                return EXIT_FAILURE;
-        }
-    }
-    // Reset optind for future use
-    const int optind_copy = optind;
-    optind = 0;
-
-
-    // Parse command
-
-    // There are remaining non-option arguments
-    if (optind_copy < argc) {
-        bool cmd_found = false;
-        char *combined_name = NULL;
-        int cmd_ret = -1;
-
-        for (size_t i = 0; i < num_cmds; ++ i) {
-            if (strcmp(argv[optind_copy], cmds[i].name) == 0) {
-                cmd_found = true;
-
-                // Combining argv[0] and the command name
-                // TODO this can be made quicker by putting argv[0] before command name and using both in command program
-                // but then it's less neat and modular
-
-                size_t argv_0_len = strlen(argv[0]);
-                size_t cmd_len = strlen(argv[optind_copy]);
-                size_t combined_len = argv_0_len + 1 + cmd_len + 1; // +2 for ' ' and '\0'
-
-                combined_name = (char *) malloc(combined_len * sizeof *combined_name);
-                MALLOC_CHK(combined_name);
-                memcpy(combined_name, argv[0], argv_0_len);
-                combined_name[argv_0_len] = ' ';
-                memcpy(combined_name + argv_0_len + 1, argv[optind_copy], cmd_len);
-                combined_name[combined_len - 1] = '\0';
-                argv[optind_copy] = combined_name;
-
-                // Calling command program
-                if (meta.verbose) {
-                    VERBOSE("using command %s", cmds[i].name);
-                }
-                cmd_ret = cmds[i].main(argc - optind_copy, argv + optind_copy, &meta);
+                default: // case '?'
+                    fprintf(stderr, HELP_SMALL_MSG, argv[0]);
+                    ret = EXIT_FAILURE;
+                    break_flag = true;
+                    break;
             }
         }
 
-        // No command found
-        if (!cmd_found) {
-            MESSAGE(stderr, "invalid command -- '%s'", argv[optind_copy]);
-            fprintf(stderr, HELP_SMALL_MSG, argv[0]);
-            EXIT_MSG(EXIT_FAILURE, argv, &meta);
-            return EXIT_FAILURE;
+        if (!break_flag) {
 
-        } else {
-            free(combined_name);
-            combined_name = NULL;
-            EXIT_MSG(cmd_ret, argv, &meta);
-            return cmd_ret;
+            // Reset optind for future use
+            const int optind_copy = optind;
+            optind = 0;
+
+            // Parse command
+
+            // There are remaining non-option arguments
+            if (optind_copy < argc) {
+                bool cmd_found = false;
+                char *combined_name = NULL;
+
+                for (size_t i = 0; i < num_cmds; ++ i) {
+                    if (strcmp(argv[optind_copy], cmds[i].name) == 0) {
+                        cmd_found = true;
+
+                        // Combining argv[0] and the command name
+                        // TODO this can be made quicker by putting argv[0] before command name and using both in command program
+                        // but then it's less neat and modular
+
+                        size_t argv_0_len = strlen(argv[0]);
+                        size_t cmd_len = strlen(argv[optind_copy]);
+                        size_t combined_len = argv_0_len + 1 + cmd_len + 1; // +2 for ' ' and '\0'
+
+                        combined_name = (char *) malloc(combined_len * sizeof *combined_name);
+                        MALLOC_CHK(combined_name);
+                        memcpy(combined_name, argv[0], argv_0_len);
+                        combined_name[argv_0_len] = ' ';
+                        memcpy(combined_name + argv_0_len + 1, argv[optind_copy], cmd_len);
+                        combined_name[combined_len - 1] = '\0';
+                        argv[optind_copy] = combined_name;
+
+                        // Calling command program
+                        if (meta.verbose) {
+                            VERBOSE("using command %s", cmds[i].name);
+                        }
+                        ret = cmds[i].main(argc - optind_copy, argv + optind_copy, &meta);
+                    }
+                }
+
+                // No command found
+                if (!cmd_found) {
+                    MESSAGE(stderr, "invalid command -- '%s'", argv[optind_copy]);
+                    fprintf(stderr, HELP_SMALL_MSG, argv[0]);
+                    ret = EXIT_FAILURE;
+
+                } else {
+                    free(combined_name);
+                    combined_name = NULL;
+                }
+
+            // No remaining non-option arguments
+            } else {
+                MESSAGE(stderr, "missing command%s", "");
+                fprintf(stderr, HELP_SMALL_MSG, argv[0]);
+                ret = EXIT_FAILURE;
+            }
         }
-
-    // No remaining non-option arguments
-    } else {
-        MESSAGE(stderr, "missing command%s", "");
-        fprintf(stderr, HELP_SMALL_MSG, argv[0]);
-        EXIT_MSG(EXIT_FAILURE, argv, &meta);
-        return EXIT_FAILURE;
     }
 
-
-
-    /* TODO uncomment
-
-    int ret=1;
-
-    if(argc<2){
-        fprintf(stderr, HELP_SMALL_MSG, argv[0]);
+    if (meta.verbose) {
+        VERBOSE("printing resource use%s", "");
     }
-    else if(strcmp(argv[1],"fastt")==0){
-        ret=fastt_main(argc-1, argv+1);
-	}
-    else if(strcmp(argv[1],"--version")==0 || strcmp(argv[1],"-V")==0){
-        fprintf(stdout,"slow5tools %s\n",SLOW5_VERSION);
-        exit(EXIT_SUCCESS);
-    }
-    else if(strcmp(argv[1],"--help")==0 || strcmp(argv[1],"-h")==0){
-        print_usage(stdout);
-    }
-    else{
-        fprintf(stderr,"[slow5tools] Unrecognised command %s\n",argv[1]);
-        print_usage(stderr);
-    }
+    MESSAGE(stderr, "real time = %.3f sec | CPU time = %.3f sec | peak RAM = %.3f GB",
+            realtime() - init_realtime, cputime(), peakrss() / 1024.0 / 1024.0 / 1024.0);
 
-    fprintf(stderr, "\n[%s] CMD:", __func__);
-    for (int i = 0; i < argc; ++i) {
-        fprintf(stderr, " %s", argv[i]);
-    }
-
-    fprintf(stderr, "\n[%s] Real time: %.3f sec; CPU time: %.3f sec; Peak RAM: %.3f GB\n\n",
-            __func__, realtime() - realtime0, cputime(),peakrss() / 1024.0 / 1024.0 / 1024.0);
-
+    EXIT_MSG(ret, argv, &meta);
     return ret;
-    */
 }
