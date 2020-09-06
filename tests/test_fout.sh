8efa29efa1f6dd8f4f9849a59d73b2a6831d7396
Sasha Jenner <jenner.sasha@gmail.com>
2020-09-06 16:13:00 +1000

    Test blow5 output

--- tests/test_fout.sh
+++ tests/test_fout.sh
@@ -11,8 +11,9 @@
 # - Expects testing datasets with the following structure:
 #     - dataset/
 #         |- expected.slow5
-#         |- expected.slow5.fti TODO change extension here
-#         |- expected_bin.slow5
+#         |- expected.slow5.s5i
+#         |- expected.blow5
+#         |- expected.blow5.b5i
 #         |- fast5_files/
 #             |- expected_1.fast5
 #             |- ...
@@ -55,7 +56,9 @@ if [ ! -f $SLOW5TOOLS_PATH ]; then
 fi
 
 CMD_FAST5_TO_SLOW5="f2s"
+CMD_FAST5_TO_BLOW5="f2s -b"
 CMD_SLOW5_IDX="index"
+CMD_BLOW5_IDX="index"
 
 
 # Folder name in datasets containing FAST5 files
@@ -66,10 +69,20 @@ SLOW5_EXPECTED="expected.slow5"
 # File name of actual SLOW5 output
 SLOW5_ACTUAL="actual.slow5"
 
+# File name of expected BLOW5 output
+BLOW5_EXPECTED="expected.blow5"
+# File name of actual BLOW5 output
+BLOW5_ACTUAL="actual.blow5"
+
 # File name of expected SLOW5 index output
 SLOW5_IDX_EXPECTED="expected.slow5.s5i"
 # File name of actual SLOW5 index output
 SLOW5_IDX_ACTUAL="actual.slow5.s5i"
+
+# File name of expected SLOW5 index output
+BLOW5_IDX_EXPECTED="expected.blow5.b5i"
+# File name of actual SLOW5 index output
+BLOW5_IDX_ACTUAL="actual.blow5.b5i"
 
 declare -i ret=0
 
@@ -77,8 +90,11 @@ declare -i ret=0
 for testset in $DATA_DIR/*; do
     echo "$testset"
 
-    "$SLOW5TOOLS_PATH" "$CMD_FAST5_TO_SLOW5" "$testset/$FAST5_FOLDER" 2>/dev/null | sort -r > "$testset/$SLOW5_ACTUAL"
+    rm "$testset/$SLOW5_ACTUAL"
     rm "$testset/$SLOW5_IDX_ACTUAL"
+
+    # Sort the reads but not the header
+    "$SLOW5TOOLS_PATH" "$CMD_FAST5_TO_SLOW5" "$testset/$FAST5_FOLDER" 2>/dev/null | awk '{if (substr($0,1,1) == "#") print $0 > "/dev/stderr"; else print $0}' 2> "$testset/$SLOW5_ACTUAL" | sort -r >> "$testset/$SLOW5_ACTUAL"
     "$SLOW5TOOLS_PATH" $CMD_SLOW5_IDX "$testset/$SLOW5_ACTUAL" 2>/dev/null
 
     if diff "$testset/$SLOW5_EXPECTED" "$testset/$SLOW5_ACTUAL" 2>&1 >/dev/null; then
@@ -92,6 +108,26 @@ for testset in $DATA_DIR/*; do
         echo "SUCCESS slow5 index"
     else
         echo "FAILED slow5 index"
+        ret=1
+    fi
+
+    rm "$testset/$BLOW5_ACTUAL"
+    rm "$testset/$BLOW5_IDX_ACTUAL"
+
+    "$SLOW5TOOLS_PATH" $CMD_FAST5_TO_BLOW5 "$testset/$FAST5_FOLDER" -o "$testset/$BLOW5_ACTUAL" 2>/dev/null
+    "$SLOW5TOOLS_PATH" "$CMD_BLOW5_IDX" "$testset/$BLOW5_ACTUAL" 2>/dev/null
+
+    if diff "$testset/$BLOW5_EXPECTED" "$testset/$BLOW5_ACTUAL" 2>&1 >/dev/null; then
+        echo "SUCCESS fast5 -> blow5"
+    else
+        echo "FAILED fast5 -> blow5"
+        ret=1
+    fi
+
+    if diff "$testset/$BLOW5_IDX_EXPECTED" "$testset/$BLOW5_IDX_ACTUAL" 2>&1 >/dev/null; then
+        echo "SUCCESS blow5 index"
+    else
+        echo "FAILED blow5 index"
         ret=1
     fi
 
