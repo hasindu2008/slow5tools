4a02fc4d90e12680e38b34b7c3a454f50f10646c
Sasha Jenner <sasjen@genometech-gpgpu.garvan.unsw.edu.au>
2020-10-29 07:53:32 +1100

    Updated to put output in /data directory

--- tests/benchmark.sh
+++ tests/benchmark.sh
@@ -2,19 +2,27 @@
 # Extract read entries given read ids
 # Benchmark over 1,2,4,8,16,32 threads
 
-REL_DIR="$(dirname $0)/"
-OUT_DIR="$REL_DIR/output/GZFN211103/"
+DATA_DIR='/data/slow5-testdata/GZFN211103/'
+OUT_DIR="$DATA_DIR/slow5/take"
 
+i=1
+while [ -e "$OUT_DIR$i" ]; do
+    i=$((i+1))
+done
+OUT_DIR="$OUT_DIR$i/"
+
+mkdir "$OUT_DIR"
 cd "$OUT_DIR"
 
-SLOW5TOOLS_PATH="../../../slow5tools"
-READID_FILE=reads.list
+SLOW5TOOLS_PATH='/home/sasjen/slow5/slow5tools'
+READID_FILE="$DATA_DIR/reads.list"
+SLOW5_FILE="$DATA_DIR/slow5/bench.blow5.gz"
 
 i=1
 while [ "$i" -le "32" ]; do
     clean_fscache
     echo "Extracting with $i threads"
-    time -v "$SLOW5TOOLS_PATH" extract "-@$i" bench.blow5.gz < "$READID_FILE" > "bench.out$i" 2> "bench.stderr$i"
+    time -v "$SLOW5TOOLS_PATH" extract "-@$i" "$SLOW5_FILE" < "$READID_FILE" > "bench.out$i" 2> "bench.stderr$i"
     i=$((i*2))
 done
 
