3ef7d7a47201344c891887895c676e1550446842
Sasha Jenner <jenner.sasha@gmail.com>
2020-10-29 20:21:53 +1100

    Script update

--- scripts/get_times.sh
+++ scripts/get_times.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
+DATA_DIR="$1"
+
 n=1
 while [ $n -le 32 ]; do
-    grep "real time" "$DATA_DIR/bench$i.stderr"
-    $n=$((n*2))
+    grep "real time" "$DATA_DIR/bench$n.stderr"
+    n=$((n*2))
 done
