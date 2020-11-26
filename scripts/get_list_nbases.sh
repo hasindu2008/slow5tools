d1db3ffeb7da7ef0a2f4f6c83d709d30dd5484e6
Sasha Jenner <jenner.sasha@gmail.com>
2020-11-27 08:12:16 +1100

    Script update

--- scripts/get_list_nbases.sh
+++ scripts/get_list_nbases.sh
@@ -4,9 +4,9 @@ READ_LIST="$1"
 READ_FASTQ="$2"
 
 nbases=0
-for readid in $(cat "$READ_LIST"); do
+while read -r readid; do
     read_bases=$(grep "$readid" "$READ_FASTQ" -m 1 -A 1 | tail -1 | wc -c)
     nbases=$((nbases + read_bases - 1))
-done
+done < "$READ_LIST"
 
 echo "$nbases"
