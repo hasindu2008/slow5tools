4478f1929d9c80c8e0533b8da0615132d6cfcdcf
Sasha Jenner <sasjen@genometech-gpgpu.garvan.unsw.edu.au>
2020-12-15 16:19:46 +1100

    Changes to script

--- scripts/get_list_nbases.sh
+++ scripts/get_list_nbases.sh
@@ -2,11 +2,15 @@
 
 READ_LIST="$1"
 READ_FASTQ="$2"
+OUT="$3"
 
 nbases=0
+i=0
 while read -r readid; do
     read_bases=$(grep "$readid" "$READ_FASTQ" -m 1 -A 1 | tail -1 | wc -c)
     nbases=$((nbases + read_bases - 1))
+    i=$((i + 1))
+    echo "$i"
 done < "$READ_LIST"
 
-echo "$nbases"
+echo "$nbases" > "$OUT"
