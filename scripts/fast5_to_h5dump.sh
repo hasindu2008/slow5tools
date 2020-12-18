2c43c1cb04f6b3eab637749ef9633007ec6e9b33
Sasha Jenner <jenner.sasha@gmail.com>
2020-12-18 15:44:13 +1100

    Little edit

--- scripts/fast5_to_h5dump.sh
+++ scripts/fast5_to_h5dump.sh
@@ -2,6 +2,9 @@
 #
 # Create h5dump equivalents of fast5 files
 # Usage: $0 fast5_folder out_folder
+#
+# If exists subfolders in fast5_folder the program prefixes
+# the fast5 files in them with the folder name followed by an underscore
 
 i=1
 # h5dump_file file/dir out_folder prefix
@@ -10,13 +13,18 @@ h5dump_file() {
         file="$1"
         file_base=$(basename "$file")
         h5dump -A "$file" > "$2/$3_$file_base.h5dump"
+        echo "$3_$file_base" # testing
         echo "$i"
         i=$((i + 1))
     elif [ -d "$1" ]; then
         dir="$1"
         dir_base="$(basename "$dir")"
         for file in "$dir"/*; do
-            h5dump_file "$file" "$2" "$3_$dir_base"
+            if [ -n "$3" ]; then
+                h5dump_file "$file" "$2" "$3_$dir_base"
+            else
+                h5dump_file "$file" "$2" "$dir_base"
+            fi
         done
     fi
 }
