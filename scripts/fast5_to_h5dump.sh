08b3ff748841c569acf00025e7c75168645131c8
Sasha Jenner <jenner.sasha@gmail.com>
2020-12-18 15:06:00 +1100

    Edit to allow sub directories

--- scripts/fast5_to_h5dump.sh
+++ scripts/fast5_to_h5dump.sh
@@ -3,16 +3,26 @@
 # Create h5dump equivalents of fast5 files
 # Usage: $0 fast5_folder out_folder
 
-if [ -n "$1" ] && [ -n "$2" ]; then
-
-    i=1
-    for file in "$1"/*; do
+i=1
+h5dump_file() {
+    if [ -f "$1" ]; then
+        file="$1"
         file_base=$(basename "$file")
-        if [ -f "$file" ]; then
-            h5dump -A "$file" > "$2/$file_base.h5dump"
-        fi
+        h5dump -A "$file" > "$2/$3_$file_base.h5dump"
         echo "$i"
         i=$((i + 1))
+    elif [ -d "$1" ]; then
+        dir="$1"
+        for file in "$dir"/*; do
+            h5dump_file "$file" "$2" "$dir"
+        done
+    fi
+}
+
+if [ -n "$1" ] && [ -n "$2" ]; then
+
+    for file in "$1"/*; do
+        h5dump_file "$file" "$2" ""
     done
 
 else
