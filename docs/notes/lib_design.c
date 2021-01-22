686e3d60ec83648dc328cc19d24bc9b9b686c95b
Sasha Jenner <jenner.sasha@gmail.com>
2021-01-22 13:55:08 +1100

    Lib design change

--- docs/notes/lib_design.c
+++ docs/notes/lib_design.c
@@ -1,10 +1,10 @@
 // open slow5 file
 // get read entry from id
 
-SLOW5File *fp = slow5_open("eg.slow5", "r");
+slow5_file *fp = slow5_open("eg.slow5", "r");
 
-SLOW5Read *read_entry1 = slow5_get("read_laks0293408", fp);
-SLOW5Read *read_entry2 = slow5_get("read_aslkdj20398", fp);
+slow5_read *read_entry1 = slow5_get("read_laks0293408", fp);
+slow5_read *read_entry2 = slow5_get("read_aslkdj20398", fp);
 slow5_read_print(read_entry2); // slow5_read_fprint(read_entry2, stdout);
 
 slow5_close(fp);
@@ -14,11 +14,11 @@ slow5_read_free(read_entry2);
 // open slow5 file
 // get read entries from ids
 
-SLOW5File *fp = slow5_open("eg.slow5", "r");
+slow5_file *fp = slow5_open("eg.slow5", "r");
 
 char *reads[] = { "readx", "ready", "readz" };
 uint64_t n = 3;
-SLOW5Read **read_entries = slow5_get_multi(reads, n, fp); // SLOW5Read **read_entries = slow5_get(reads, n, fp);
+slow5_read **read_entries = slow5_get_multi(reads, n, fp); // slow5_read **read_entries = slow5_get(reads, n, fp);
 
 slow5_close(fp);
 slow5_read_free(read_entry1);
@@ -26,10 +26,10 @@ slow5_read_free(read_entry2);
 
 // merge both two slow5 files
 
-SLOW5File *fp1 = slow5_open("eg1.slow5", "r");
-SLOW5File *fp2 = slow5_open("eg2.slow5", "r");
+slow5_file *fp1 = slow5_open("eg1.slow5", "r");
+slow5_file *fp2 = slow5_open("eg2.slow5", "r");
 
-SLOW5File *fpout = slow5_open("egout.slow5", "w");
+slow5_file *fpout = slow5_open("egout.slow5", "w");
 slow5_merge(fp1, fp2, fpout);
 
 slow5_close(fp1);
@@ -38,10 +38,10 @@ slow5_close(fpout);
 
 // merge both two slow5 files. write to stdout
 
-SLOW5File *fp1 = slow5_open("eg1.slow5", "r");
-SLOW5File *fp2 = slow5_open("eg2.slow5", "r");
+slow5_file *fp1 = slow5_open("eg1.slow5", "r");
+slow5_file *fp2 = slow5_open("eg2.slow5", "r");
 
-SLOW5File *fpout = slow5_init_fp(stdout, FORMAT_SLOW5);
+slow5_file *fpout = slow5_init_fp(stdout, FORMAT_SLOW5);
 slow5_merge(fp1, fp2, fpout);
 
 fclose(fp1);
@@ -49,10 +49,10 @@ fclose(fp2);
 
 // merge both two slow5 files. write to fd
 
-SLOW5File *fp1 = slow5_open("eg1.slow5", "r");
-SLOW5File *fp2 = slow5_open("eg2.slow5", "r");
+slow5_file *fp1 = slow5_open("eg1.slow5", "r");
+slow5_file *fp2 = slow5_open("eg2.slow5", "r");
 
-SLOW5File *fpout = slow5_init_fd(1, FORMAT_SLOW5);
+slow5_file *fpout = slow5_init_fd(1, FORMAT_SLOW5);
 slow5_merge(fp1, fp2, fpout);
 
 fclose(fp1);
@@ -60,8 +60,8 @@ fclose(fp2);
 
 // convert slow5 to blow5 file
 
-SLOW5File *fp1 = slow5_open("eg1.slow5", "r");
-SLOW5File *fp2 = slow5_open("eg1.blow5", "w");
+slow5_file *fp1 = slow5_open("eg1.slow5", "r");
+slow5_file *fp2 = slow5_open("eg1.blow5", "w");
 
 slow5_write(fp1, fp2);
 
@@ -70,9 +70,9 @@ slow5_close(fp2);
 
 // convert slow5 to two blow5 files with different compression
 
-SLOW5File *fp1 = slow5_open("eg1.slow5", "r");
-SLOW5File *fp2 = slow5_open_with("eg1.blow5", "w+", COMPRESS_NONE);
-SLOW5File *fp3 = slow5_init_fp(stdout, "w", FORMAT_BINARY | COMPRESS_GZIP);
+slow5_file *fp1 = slow5_open("eg1.slow5", "r");
+slow5_file *fp2 = slow5_open_with("eg1.blow5", "w+", COMPRESS_NONE);
+slow5_file *fp3 = slow5_init_fp(stdout, "w", FORMAT_BINARY | COMPRESS_GZIP);
 
 slow5_write(fp1, fp2);
 slow5_write(fp1, fp3); // slow5_write(fp2, fp3); should work too
@@ -83,7 +83,7 @@ slow5_free(fp3);
 
 // convert fast5 to slow5 file
 
-SLOW5File *out = slow5_open("new.blow5", "w");
+slow5_file *out = slow5_open("new.blow5", "w");
 
 fast5_to_slow5("fast5_files/", out);
 
@@ -91,7 +91,7 @@ slow5_close(out);
 
 // convert slow5 to fast5 files
 
-SLOW5File *in = slow5_open("in.slow5", "r");
+slow5_file *in = slow5_open("in.slow5", "r");
 
 slow5_to_fast5(in, "fast5_files/");
 
@@ -99,7 +99,7 @@ slow5_close(in);
 
 // split a slow5 file by read group
 
-SLOW5File *in = slow5_open("in.slow5", "r");
+slow5_file *in = slow5_open("in.slow5", "r");
 
 slow5_split(in, "slow5_files/");
 
