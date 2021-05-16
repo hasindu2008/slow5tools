00e7b430abe22a7771c1a5a9103b92a4f570f555
hiruna7 <h.samarakoon@20face.com>
2021-05-16 11:44:09 +0530

    f2s using different slow5_format

--- test/f2s_s2f_integrity_test.sh
+++ test/f2s_s2f_integrity_test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Run f2s, s2f, and again f2s and check if first produced slow5s are same as the last set.
-Usage="f2s_s2f_integrity_test.sh [path to fast5 directory] [path to create a temporary directory] [path to slow5tools executable]"
+Usage="f2s_s2f_integrity_test.sh [path to fast5 directory] [path to create a temporary directory] [path to slow5tools executable] [-c or -s (optional)]"
 
 if [[ "$#" -lt 3 ]]; then
 	echo "Usage: $Usage"
@@ -18,6 +18,13 @@ F2S_atm1_OUTPUT="$TEMP_DIR/f2s_attempt1"
 S2F_OUTPUT="$TEMP_DIR/s2f"
 F2S_atm2_OUTPUT="$TEMP_DIR/f2s_attempt2"
 
+
+SLOW5_FORMAT=""
+if [[ "$#" -eq 4 ]]; then
+    SLOW5_FORMAT=$4
+fi
+
+
 rm -r "$TEMP_DIR"
 mkdir "$TEMP_DIR" || exit 1
 mkdir "$F2S_atm1_OUTPUT" || exit 1
@@ -27,7 +34,7 @@ mkdir "$F2S_atm2_OUTPUT" || exit 1
 clean_fscache
 echo "-------------------f2s attempt 1-------------------"
 echo
-if ! $SLOW5_EXEC f2s $FAST5_DIR -d $F2S_atm1_OUTPUT --iop 64 -s; then
+if ! $SLOW5_EXEC f2s $FAST5_DIR -d $F2S_atm1_OUTPUT --iop 64 $SLOW5_FORMAT; then
     echo "f2s attempt 1 failed" 
     exit 1
 fi
@@ -45,7 +52,7 @@ clean_fscache
 echo
 echo "-------------------f2s attempt 2-------------------"
 echo
-if ! $SLOW5_EXEC f2s $S2F_OUTPUT -d $F2S_atm2_OUTPUT --iop 64 -s; then
+if ! $SLOW5_EXEC f2s $S2F_OUTPUT -d $F2S_atm2_OUTPUT --iop 64 $SLOW5_FORMAT; then
     echo "f2s attempt 2 failed"
     exit 1
 fi
