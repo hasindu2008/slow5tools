64b6f6636a50f0712d4ed7e2a041cf1d54bd9308
hiruna7 <h.samarakoon@20face.com>
2021-06-03 22:47:06 +0530

    debug 7

--- test/f2s_output_test.sh
+++ test/f2s_output_test.sh
@@ -148,6 +148,7 @@ echo -e "${GREEN}testcase passed${NC}"
 
 echo
 rm $OUTPUT_DIR/stdout.slow5
+#input:directory on a single process cannot be tested as the order of the files read by the program can vary.
 echo "------------------- f2s testcase 9 >>> format:single_and_multi-fast5 input:file process:single_process output:stdout-------------------"
 if ! $SLOW5_EXEC f2s $FAST5_DIR/single-and-multi-fast5/1ssm1.fast5 $FAST5_DIR/single-and-multi-fast5/2sss1.fast5 $FAST5_DIR/single-and-multi-fast5/3ssm2.fast5 $FAST5_DIR/single-and-multi-fast5/4sss2.fast5 --iop 1 --to slow5 > $OUTPUT_DIR/stdout.slow5; then
     echo "${RED}testcase failed${NC}" 
