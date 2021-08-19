16f4116aa71d1e23291fc0ae9d95e533321febda
hiruna72 <hiruna72@gmail.com>
2021-08-19 08:58:47 +0530

    print output dir details f2s_s2f_integrity_test

--- test/f2s_s2f_integrity_test.sh
+++ test/f2s_s2f_integrity_test.sh
@@ -39,7 +39,7 @@ if [[ "$#" -eq 4 ]]; then
     SLOW5_FORMAT=$4
 fi
 
-test -d $TEMP_DIR && rm -r "$TEMP_DIR"
+test -d "$TEMP_DIR" && rm -r "$TEMP_DIR"
 mkdir "$TEMP_DIR" || die "Creating $TEST_DIR failed"
 mkdir "$F2S_atm1_OUTPUT" || die "Creating $F2S_atm1_OUTPUT failed"
 mkdir "$S2F_OUTPUT" || die "Creating $S2F_OUTPUT failed"
@@ -51,23 +51,31 @@ if [[ $* == *-f* ]];then
 fi
 echo "-------------------f2s attempt 1-------------------"
 echo
-$SLOW5_EXEC f2s $FAST5_DIR -d $F2S_atm1_OUTPUT --iop 64 $SLOW5_FORMAT 2>/dev/null || die "f2s attempt 1 failed"
+$SLOW5_EXEC f2s "$FAST5_DIR" -d "$F2S_atm1_OUTPUT" --iop 64 $SLOW5_FORMAT 2>/dev/null || die "f2s attempt 1 failed"
 if [[ $* == *-f* ]];then
   clean_fscache
 fi
 echo
 echo "-------------------s2f attempt-------------------"
 echo
-$SLOW5_EXEC s2f $F2S_atm1_OUTPUT -d $S2F_OUTPUT --iop 64 2>/dev/null || die "s2f failed"
+$SLOW5_EXEC s2f "$F2S_atm1_OUTPUT" -d "$S2F_OUTPUT" --iop 64 2>/dev/null || die "s2f failed"
 if [[ $* == *-f* ]];then
   clean_fscache
 fi
 echo
 echo "-------------------f2s attempt 2-------------------"
 echo
-$SLOW5_EXEC f2s $S2F_OUTPUT -d $F2S_atm2_OUTPUT --iop 64 $SLOW5_FORMAT 2>/dev/null || die "f2s attempt 2 failed"
+$SLOW5_EXEC f2s "$S2F_OUTPUT" -d "$F2S_atm2_OUTPUT" --iop 64 $SLOW5_FORMAT 2>/dev/null || die "f2s attempt 2 failed"
 echo "running diff on f2s attempt 1 and f2s attempt 2"
-diff -s $F2S_atm1_OUTPUT $F2S_atm2_OUTPUT &>/dev/null
+echo "du -hs $F2S_atm1_OUTPUT"
+du -hs "$F2S_atm1_OUTPUT"
+echo "du -hs $F2S_atm2_OUTPUT"
+du -hs "$F2S_atm2_OUTPUT"
+echo "ls $F2S_atm1_OUTPUT | wc"
+ls "$F2S_atm1_OUTPUT" | wc
+echo "ls $F2S_atm2_OUTPUT | wc"
+ls "$F2S_atm2_OUTPUT" | wc
+diff -s "$F2S_atm1_OUTPUT" "$F2S_atm2_OUTPUT" &>/dev/null
 
 if [ $? -eq 0 ]; then
     echo -e "${GREEN}SUCCESS: f2s and s2f conversions are consistent!${NC}"
