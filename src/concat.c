37fb6336cb3e1bc25aaf7ebc4b29680f7035070f
hiruna72 <hiruna72@gmail.com>
2021-08-24 20:28:23 +0530

    concat made simpler

--- src/concat.c
+++ src/concat.c
@@ -23,9 +23,6 @@
     "\n" \
     "OPTIONS:\n"       \
     "    -o, --output [FILE]                output contents to FILE [default: stdout]\n" \
-    "    -l, --lossless [STR]               concat files with auxilliary fields.[default: true].\n" \
-    "    --format                           concat file format [default:.blow5]\n"\
-    "    -c, --compress [compression_type]  concat compressed blow5 [default: zlib]\n" \
     "    -h, --help                         display this message and exit\n" \
 
 int concat_main(int argc, char **argv, struct program_meta *meta){
@@ -58,66 +55,29 @@ int concat_main(int argc, char **argv, struct program_meta *meta){
     static struct option long_opts[] = {
             {"help", no_argument, NULL, 'h' }, //0
             {"output", required_argument, NULL, 'o'}, //1
-            { "lossless", required_argument, NULL, 'l'}, //2
-            { "format", required_argument, NULL, 'f'}, //3
-            {"compress", required_argument, NULL, 'c'},  //4
             {NULL, 0, NULL, 0 }
     };
 
     // Input arguments
     char *arg_fname_out = NULL;
-    std::string format =  ".blow5";
+    std::string format =  "low5";
     int lossy = 0;
 
     enum slow5_fmt format_out = SLOW5_FORMAT_BINARY;
-    enum slow5_press_method pressMethod = SLOW5_COMPRESS_ZLIB;
-    int compression_set = 0;
+    slow5_press_method_t pressMethod = SLOW5_COMPRESS_ZLIB;
 
     int longindex = 0;
     int opt;
 
     // Parse options
-    while ((opt = getopt_long(argc, argv, "ho:l:f:c:", long_opts, &longindex)) != -1) {
+    while ((opt = getopt_long(argc, argv, "ho:", long_opts, &longindex)) != -1) {
         if (meta->verbosity_level >= LOG_DEBUG) {
             DEBUG("opt='%c', optarg=\"%s\", optind=%d, opterr=%d, optopt='%c'",
                   opt, optarg, optind, opterr, optopt);
         }
         switch (opt) {
-            case 'l':
-                if(strcmp(optarg,"true")==0){
-                    lossy = 0;
-                }else if(strcmp(optarg,"false")==0){
-                    lossy = 1;
-                }else{
-                    ERROR("Incorrect argument%s", "");
-                    return EXIT_FAILURE;
-                }
-                break;
             case 'o':
                 arg_fname_out = optarg;
-                break;
-            case 'f':
-                if(strcmp(optarg,"slow5")==0){
-                    format_out = SLOW5_FORMAT_ASCII;
-                    format = ".slow5";
-                }else if(strcmp(optarg,"blow5")==0){
-                    format_out = SLOW5_FORMAT_BINARY;
-                    format = ".blow5";
-                }else{
-                    ERROR("Incorrect file format%s", "");
-                    return EXIT_FAILURE;
-                }
-                break;
-            case 'c':
-                compression_set = 1;
-                if(strcmp(optarg,"none")==0){
-                    pressMethod = SLOW5_COMPRESS_NONE;
-                }else if(strcmp(optarg,"zlib")==0){
-                    pressMethod = SLOW5_COMPRESS_ZLIB;
-                }else{
-                    ERROR("Incorrect compression type%s", "");
-                    return EXIT_FAILURE;
-                }
                 break;
             case 'h':
                 if (meta->verbosity_level >= LOG_DEBUG) {
@@ -132,32 +92,12 @@ int concat_main(int argc, char **argv, struct program_meta *meta){
                 return EXIT_FAILURE;
         }
     }
-    if(compression_set == 0 && format_out == SLOW5_FORMAT_ASCII){
-        pressMethod = SLOW5_COMPRESS_NONE;
-    }
-    // compression option is only effective with -b blow5
-    if(compression_set == 1 && format_out == SLOW5_FORMAT_ASCII){
-        ERROR("%s","Compression option (-c) is only available for SLOW5 binary format.");
-        return EXIT_FAILURE;
-    }
 
     std::string output_file;
     std::string extension = format;
     if(arg_fname_out){
         output_file = std::string(arg_fname_out);
         extension = output_file.substr(output_file.length()-6, output_file.length());
-    }
-
-    if(arg_fname_out && format_out==SLOW5_FORMAT_ASCII && extension!=".slow5"){
-        ERROR("Output file extension '%s' does not match with the input file format:FORMAT_ASCII", extension.c_str());
-        fprintf(stderr, HELP_SMALL_MSG, argv[0]);
-        EXIT_MSG(EXIT_FAILURE, argv, meta);
-        return EXIT_FAILURE;
-    }else if(arg_fname_out && format_out==SLOW5_FORMAT_BINARY && extension!=".blow5"){
-        ERROR("Output file extension '%s' does not match with the input file format:FORMAT_BINARY", extension.c_str());
-        fprintf(stderr, HELP_SMALL_MSG, argv[0]);
-        EXIT_MSG(EXIT_FAILURE, argv, meta);
-        return EXIT_FAILURE;
     }
 
     // Check for remaining files to parse
@@ -193,135 +133,130 @@ int concat_main(int argc, char **argv, struct program_meta *meta){
     }
 
     slow5_file_t* slow5File = slow5_init_empty(slow5_file_pointer, arg_fname_out, format_out);
-    slow5_hdr_initialize(slow5File->header, lossy);
-    slow5File->header->num_read_groups = 0;
 
-    int header_write = 1;
-    int run_id_capture = 1;
-    std::string run_id;
+
+    int first_iteration = 1;
+    uint32_t num_read_groups = 1;
+    std::vector<std::string> run_ids;
+
     size_t num_files = slow5_files.size();
     for(size_t i=0; i<num_files; i++) { //iterate over slow5files
-        slow5_file_t *slow5File_i = slow5_open(slow5_files[i].c_str(), "r+");
+        slow5_file_t *slow5File_i = slow5_open(slow5_files[i].c_str(), "r");
         if (!slow5File_i) {
             ERROR("[Skip file]: cannot open %s. skipping...\n", slow5_files[i].c_str());
-            continue;
+            return EXIT_FAILURE;
         }
-        if (lossy == 0 && slow5File_i->header->aux_meta == NULL) {
-            WARNING("[Skip file]: %s has no auxiliary fields. Use merge (instead of concat) to merge files.",
-                  slow5_files[i].c_str());
-            slow5_close(slow5File_i);
-            continue;
-        }
-        if (lossy == 1 && slow5File_i->header->aux_meta != NULL) {
-            WARNING("[Skip file]: %s has auxiliary fields. Use merge (instead of concat) to merge files.",
-                    slow5_files[i].c_str());
-            slow5_close(slow5File_i);
-            continue;
-        }
-        if (slow5File_i->format != format_out) {
-            WARNING("[Skip file]: %s has a different file format. Use merge (instead of concat) to merge files.",
-                    slow5_files[i].c_str());
-            slow5_close(slow5File_i);
-            continue;
-        }
-        if (slow5File_i->compress->method != pressMethod) {
-            WARNING("[Skip file]: %s has a different compression type. Use merge (instead of concat) to merge files.",
-                    slow5_files[i].c_str());
-            slow5_close(slow5File_i);
-            continue;
-        }
-        if(run_id_capture){
-            run_id_capture = 0;
-            char* temp = slow5_hdr_get("run_id", 0, slow5File_i->header);
-            if(!temp){
-                ERROR("No run_id information found in %s.", slow5_files[i].c_str());
-                return EXIT_FAILURE;
+        if(first_iteration){
+            // set parameters
+            if (slow5File_i->header->aux_meta == NULL) {
+                lossy = 1;
             }
-            run_id = std::string(temp);
+            format_out = slow5File_i->format;
+            pressMethod = slow5File_i->compress->method;
+            num_read_groups = slow5File_i->header->num_read_groups;
 
-        }else{
-            char* run_id_check = slow5_hdr_get("run_id", 0, slow5File_i->header);
-            if(!run_id_check){
-                ERROR("No run_id information found in %s.", slow5_files[i].c_str());
+            if(arg_fname_out && format_out==SLOW5_FORMAT_ASCII && extension!=".slow5"){
+                ERROR("Output file extension '%s' does not match with the input file format:FORMAT_ASCII", extension.c_str());
+                fprintf(stderr, HELP_SMALL_MSG, argv[0]);
+                EXIT_MSG(EXIT_FAILURE, argv, meta);
+                return EXIT_FAILURE;
+            }else if(arg_fname_out && format_out==SLOW5_FORMAT_BINARY && extension!=".blow5"){
+                ERROR("Output file extension '%s' does not match with the input file format:FORMAT_BINARY", extension.c_str());
+                fprintf(stderr, HELP_SMALL_MSG, argv[0]);
+                EXIT_MSG(EXIT_FAILURE, argv, meta);
                 return EXIT_FAILURE;
             }
-            if(strcmp(run_id_check,run_id.c_str())){
-                WARNING("[Skip file]: %s has a different run_id. Use merge (instead of concat) to merge files.",
-                        slow5_files[i].c_str());
-                slow5_close(slow5File_i);
-                continue;
-            }
-        }
-        if (slow5File_i->compress->method != pressMethod) {
-            WARNING("[Skip file]: %s has a different compression type. Use merge (instead of concat) to merge files.",
-                    slow5_files[i].c_str());
-            slow5_close(slow5File_i);
-            continue;
-        }
 
-        if(header_write){
-            header_write = 0;
-            khash_t(slow5_s2s) *rg = slow5_hdr_get_data(0, slow5File_i->header); // extract jth read_group related data from ith slow5file
-            int64_t new_read_group = slow5_hdr_add_rg_data(slow5File->header, rg); //assumption0
-            if(new_read_group != 0){ //sanity check
-                ERROR("New read group number is not equal to 0. something's wrong\n%s", "");
-                return EXIT_FAILURE;
+            //set run_ids
+            for(uint32_t j=0; j<num_read_groups; j++){
+                char* temp = slow5_hdr_get("run_id", 0, slow5File_i->header);
+                if(!temp){
+                    ERROR("No run_id information found in %s.", slow5_files[i].c_str());
+                    return EXIT_FAILURE;
+                }
+                run_ids.push_back(std::string(temp));
+                //write header
+                khash_t(slow5_s2s) *rg = slow5_hdr_get_data(j, slow5File_i->header); // extract jth read_group related data from ith slow5file
+                if(!rg){
+                    ERROR("Could not fetch the header data map.%s","");
+                }
+                int64_t new_read_group = slow5_hdr_add_rg_data(slow5File->header, rg); //assumption0: if run_ids are similar the rest of the header attribute values of jth read_groups to follow will be similar.
+                if(new_read_group<0){
+                    ERROR("Could not assign a new read group. %s","");
+                    return EXIT_FAILURE;
+                }
             }
+            slow5_hdr_initialize(slow5File->header, lossy);
+            slow5File->header->num_read_groups = num_read_groups;
             //now write the header to the slow5File.
             if(slow5_hdr_fwrite(slow5File->fp, slow5File->header, format_out, pressMethod) == -1){
                 ERROR("Could not write the header to %s\n", arg_fname_out);
+                slow5_close(slow5File_i);
                 return EXIT_FAILURE;
+            }
+
+            first_iteration = 0;
+        }else {
+            if (lossy == 0 && slow5File_i->header->aux_meta == NULL) {
+                ERROR("%s has no auxiliary fields. Use merge (instead of concat) to merge files.",
+                      slow5_files[i].c_str());
+                slow5_close(slow5File_i);
+                return EXIT_FAILURE;
+            }
+            if (lossy == 1 && slow5File_i->header->aux_meta != NULL) {
+                ERROR("%s has auxiliary fields. Use merge (instead of concat) to merge files.",
+                      slow5_files[i].c_str());
+                slow5_close(slow5File_i);
+                continue;
+            }
+            if (slow5File_i->format != format_out) {
+                ERROR("%s has a different file format. Use merge (instead of concat) to merge files.",
+                      slow5_files[i].c_str());
+                slow5_close(slow5File_i);
+                return EXIT_FAILURE;
+            }
+            if (slow5File_i->compress->method != pressMethod) {
+                ERROR("%s has a different compression type. Use merge (instead of concat) to merge files.",
+                      slow5_files[i].c_str());
+                slow5_close(slow5File_i);
+                return EXIT_FAILURE;
+            }
+            if (slow5File_i->header->num_read_groups != num_read_groups) {
+                ERROR("%s has a different number of read groups than the first file. Use merge (instead of concat) to merge files.",
+                      slow5_files[i].c_str());
+                slow5_close(slow5File_i);
+                return EXIT_FAILURE;
+            }
+
+            for (uint32_t j = 0; j < num_read_groups; j++) {
+                char *temp = slow5_hdr_get("run_id", 0, slow5File_i->header);
+                if (!temp) {
+                    ERROR("No run_id information found in %s.", slow5_files[i].c_str());
+                    return EXIT_FAILURE;
+                }
+                if (strcmp(temp, run_ids[j].c_str())) {
+                    ERROR("%s has a different run_id. Use merge (instead of concat) to merge files.",
+                          slow5_files[i].c_str());
+                    slow5_close(slow5File_i);
+                    return EXIT_FAILURE;
+                }
             }
         }
-        if(format_out==SLOW5_FORMAT_ASCII) {
-            // BUFSIZE default is 8192 bytes
-            //writing to reads to the output
-            // BUFSIZE of 1 means one chareter at time
-            // good values should fit to blocksize, like 1024 or 4096
-            // higher values reduce number of system calls
-            char buf[BUFSIZ];
-            size_t size;
-            while ((size = fread(buf, 1, BUFSIZ, slow5File_i->fp))) {
-                fwrite(buf, 1, size, slow5File->fp);
-            }
-            slow5_close(slow5File_i);
-        }else{
-//            DEBUG("%d",101);
-            int64_t pos = ftell(slow5File_i->fp);
-            fseek(slow5File_i->fp, -5, SEEK_END); //slow5 EOF file "5WOLB"
-            off_t  length = ftell(slow5File_i->fp);
-            if(ftruncate(fileno(slow5File_i->fp),length) != 0){
-                ERROR("Truncating file %s failed.", slow5_files[i].c_str());
-                return EXIT_FAILURE;
-            }
-            fseek(slow5File_i->fp, pos, SEEK_SET);
 
-            // BUFSIZE default is 8192 bytes
-            //writing to reads to the output
-            // BUFSIZE of 1 means one chareter at time
-            // good values should fit to blocksize, like 1024 or 4096
-            // higher values reduce number of system calls
-            char buf[BUFSIZ];
-            size_t size;
-            while ((size = fread(buf, 1, BUFSIZ, slow5File_i->fp))) {
-                fwrite(buf, 1, size, slow5File->fp);
-            }
-            slow5_close(slow5File_i);
+        //writing to reads to the output
+        // BUFSIZE default is 8192 bytes
+        // BUFSIZE of 1 means one chareter at time
+        // good values should fit to blocksize, like 1024 or 4096
+        // higher values reduce number of system calls
+        char buf[BUFSIZ];
+        size_t size;
+        while ((size = fread(buf, 1, BUFSIZ, slow5File_i->fp))) {
+            fwrite(buf, 1, size, slow5File->fp);
+        }
+        slow5_close(slow5File_i);
 
-//            struct slow5_rec *read = NULL;
-//            struct slow5_press* compress = slow5_press_init(pressMethod);
-//            int ret;
-//            while ((ret = slow5_get_next(&read, slow5File_i)) >= 0) {
-//                if (slow5_rec_fwrite(slow5File->fp, read, slow5File->header->aux_meta, format_out, compress) == -1) {
-//                    slow5_rec_free(read);
-//                    ERROR("Could not write records to the file %s\n", arg_fname_out);
-//                    exit(EXIT_FAILURE);
-//                }
-//            }
-//            slow5_rec_free(read);
-//            slow5_press_free(compress);
-//            slow5_close(slow5File_i);
-
+        if(format_out==SLOW5_FORMAT_BINARY){
+            fseek(slow5File->fp, -5, SEEK_END); //slow5 EOF file "5WOLB"
         }
     }
 
