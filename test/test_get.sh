c2019a7eb636c7d127de58903452fa3015c2bddc
Hasindu Gamaarachchi <hasindu2008@gmail.com>
2021-09-10 20:26:15 +1000

    a few more  testcases to get and view

--- test/test_get.sh
+++ test/test_get.sh
@@ -3,7 +3,7 @@
 Usage="get_test.sh"
 
 # Relative path to "slow5/tests/"
-REL_PATH="$(dirname $0)/" 
+REL_PATH="$(dirname $0)/"
 
 NC='\033[0m' # No Color
 RED='\033[0;31m'
@@ -40,8 +40,8 @@ echo
 TESTCASE=1
 echo
 echo "------------------- slow5tools get testcase $TESTCASE -------------------"
-$SLOW5_EXEC get "$SLOW5_DIR/example2.slow5" r1 --to slow5 > "$OUTPUT_DIR/extracted_reads.txt" || die "testcase $TESTCASE failed"
-diff -s "$EXP_SLOW5_DIR/expected_extracted_reads.txt" "$OUTPUT_DIR/extracted_reads.txt" &>/dev/null
+$SLOW5_EXEC get "$SLOW5_DIR/example2.slow5" r1 --to slow5 > "$OUTPUT_DIR/extracted_reads.slow5" || die "testcase $TESTCASE failed"
+diff -s "$EXP_SLOW5_DIR/expected_extracted_reads.slow5" "$OUTPUT_DIR/extracted_reads.slow5" &>/dev/null
 if [ $? -ne 0 ]; then
     echo -e "${RED}ERROR: diff failed for 'slow5tools get testcase $TESTCASE'${NC}"
     exit 1
@@ -51,8 +51,8 @@ echo -e "${GREEN}testcase $TESTCASE passed${NC}"
 TESTCASE=2
 echo
 echo "------------------- slow5tools get testcase $TESTCASE -------------------"
-$SLOW5_EXEC get "$SLOW5_DIR/example2.slow5" r1 r5 r3 --to slow5 > "$OUTPUT_DIR/extracted_reads2.txt" || die "testcase $TESTCASE failed"
-diff -s "$EXP_SLOW5_DIR/expected_extracted_reads2.txt" "$OUTPUT_DIR/extracted_reads2.txt" &>/dev/null
+$SLOW5_EXEC get "$SLOW5_DIR/example2.slow5" r1 r5 r3 --to slow5 > "$OUTPUT_DIR/extracted_reads2.slow5" || die "testcase $TESTCASE failed"
+diff -s "$EXP_SLOW5_DIR/expected_extracted_reads2.slow5" "$OUTPUT_DIR/extracted_reads2.slow5" &>/dev/null
 if [ $? -ne 0 ]; then
     echo -e "${RED}ERROR: diff failed for 'slow5tools get testcase $TESTCASE'${NC}"
     exit 1
@@ -62,8 +62,8 @@ echo -e "${GREEN}testcase $TESTCASE passed${NC}"
 TESTCASE=3
 echo
 echo "------------------- slow5tools get testcase $TESTCASE -------------------"
-$SLOW5_EXEC get "$SLOW5_DIR/example2.slow5" --list "$SLOW5_DIR/list.txt" --to slow5 > "$OUTPUT_DIR/extracted_reads3.txt" || die "testcase $TESTCASE failed"
-diff -s "$EXP_SLOW5_DIR/expected_extracted_reads3.txt" "$OUTPUT_DIR/extracted_reads3.txt" &>/dev/null
+$SLOW5_EXEC get "$SLOW5_DIR/example2.slow5" --list "$SLOW5_DIR/list.txt" --to slow5 > "$OUTPUT_DIR/extracted_reads3.slow5" || die "testcase $TESTCASE failed"
+diff -s "$EXP_SLOW5_DIR/expected_extracted_reads3.slow5" "$OUTPUT_DIR/extracted_reads3.slow5" &>/dev/null
 if [ $? -ne 0 ]; then
     echo -e "${RED}ERROR: diff failed for 'slow5tools get testcase $TESTCASE'${NC}"
     exit 1
@@ -73,13 +73,36 @@ echo -e "${GREEN}testcase $TESTCASE passed${NC}"
 TESTCASE=4
 echo
 echo "------------------- slow5tools get testcase $TESTCASE -------------------"
-$SLOW5_EXEC get "$SLOW5_DIR/example2.slow5" -t 2 r1 r5 r3 --to slow5 > "$OUTPUT_DIR/extracted_reads2.txt" || die "testcase $TESTCASE failed"
-diff -s "$EXP_SLOW5_DIR/expected_extracted_reads2.txt" "$OUTPUT_DIR/extracted_reads2.txt" &>/dev/null
+$SLOW5_EXEC get "$SLOW5_DIR/example2.slow5" -t 2 r1 r5 r3 --to slow5 > "$OUTPUT_DIR/extracted_reads2.slow5" || die "testcase $TESTCASE failed"
+diff -s "$EXP_SLOW5_DIR/expected_extracted_reads2.slow5" "$OUTPUT_DIR/extracted_reads2.slow5" &>/dev/null
 if [ $? -ne 0 ]; then
     echo -e "${RED}ERROR: diff failed for 'slow5tools get testcase $TESTCASE'${NC}"
     exit 1
 fi
 echo -e "${GREEN}testcase $TESTCASE passed${NC}"
+
+TESTCASE=5
+echo
+echo "------------------- slow5tools get testcase $TESTCASE -------------------"
+$SLOW5_EXEC get "$SLOW5_DIR/example2.slow5"  r1 r5 r3 --to blow5 -c zlib -s none > "$OUTPUT_DIR/extracted_reads2.blow5" || die "testcase $TESTCASE failed"
+diff -s "$EXP_SLOW5_DIR/expected_extracted_reads.blow5" "$OUTPUT_DIR/extracted_reads2.blow5" &>/dev/null
+if [ $? -ne 0 ]; then
+    echo -e "${RED}ERROR: diff failed for 'slow5tools get testcase $TESTCASE'${NC}"
+    exit 1
+fi
+echo -e "${GREEN}testcase $TESTCASE passed${NC}"
+
+TESTCASE=6
+echo
+echo "------------------- slow5tools get testcase $TESTCASE -------------------"
+$SLOW5_EXEC get "$SLOW5_DIR/example2.slow5" r1 r5 r3 -o "$OUTPUT_DIR/extracted_reads2.blow5"  -c zlib -s none  || die "testcase $TESTCASE failed"
+diff -s "$EXP_SLOW5_DIR/expected_extracted_reads.blow5" "$OUTPUT_DIR/extracted_reads2.blow5" &>/dev/null
+if [ $? -ne 0 ]; then
+    echo -e "${RED}ERROR: diff failed for 'slow5tools get testcase $TESTCASE'${NC}"
+    exit 1
+fi
+echo -e "${GREEN}testcase $TESTCASE passed${NC}"
+
 
 rm -r $OUTPUT_DIR || die "Removing $OUTPUT_DIR failed"
 exit 0
